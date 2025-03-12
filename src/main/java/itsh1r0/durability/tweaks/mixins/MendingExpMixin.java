c1dd318ec288ed8dc3947d44276cd893d2a78c19
ItsukaHiro <itsukahiro.personal@proton.me>
2025-03-12 19:31:15 +0700

    remove unuse dependences

--- src/main/java/itsh1r0/durability/tweaks/mixins/MendingExpMixin.java
+++ src/main/java/itsh1r0/durability/tweaks/mixins/MendingExpMixin.java
@@ -6,7 +6,6 @@ import net.minecraft.world.entity.EquipmentSlot;
 import net.minecraft.world.entity.ExperienceOrb;
 import net.minecraft.world.entity.player.Player;
 import net.minecraft.world.item.ItemStack;
-import net.minecraft.world.item.enchantment.EnchantmentHelper;
 import net.minecraft.world.item.enchantment.Enchantments;
 import org.objectweb.asm.Opcodes;
 import org.spongepowered.asm.mixin.Mixin;
